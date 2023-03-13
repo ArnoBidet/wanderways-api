7fa3bb8030b58a3136bd137adaa09b0ec9429fe5
Arno Bidet <arno.bidet479@gmail.com>
2023-03-13 22:49:29 +0100

    main[ADD] : ajout des scripts de création et suppression des bases postgresql

---
+++ sql/drop.sql
@@ -0,0 +1,14 @@
+DROP TABLE game_statistics;
+DROP TABLE success_or_give_up_statistics;
+DROP TABLE gamemode_statistics;
+DROP TABLE map_statistics;
+DROP TABLE map_data;
+DROP TABLE data;
+DROP TABLE gamemode;
+DROP TABLE gamemode_map;
+DROP TABLE tag_map;
+DROP TABLE tag;
+DROP TABLE tag_group;
+DROP TABLE map;
+DROP TABLE translations;
+DROP TABLE languages;
