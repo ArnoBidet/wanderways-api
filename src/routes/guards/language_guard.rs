f1a70d5384921d6db118fdf4e7d2b3aefd654f6a
ArnoBidet <arno.bidet479@gmail.com>
2023-03-30 22:46:14 +0200

    added proper implementation of language extractor from header Accept-language

--- src/routes/guards/language_guard.rs
+++ src/routes/guards/language_guard.rs
@@ -80,3 +80,16 @@ mod get_lang_tests {
         assert_eq!(get_lang(""), String::from("en-US"))
     }
 }
+
+// cannot be easily tested because mocking Request is tremendously hard, moreover it's mostly 3rd party code
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Language {
+    type Error = ();
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        // Returns the language if given. If no pattern recognized, returns en-US as default.
+        match req.headers().get_one("Accept-Language") {
+            None => Outcome::Success(Language(String::from(DEFAULT_LANGUAGE))),
+            Some(lang) => Outcome::Success(Language(get_lang(lang))),
+        }
+    }
+}
