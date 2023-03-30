680cadabe63679f92c9064e5f9f948db479b3138
ArnoBidet <arno.bidet479@gmail.com>
2023-03-30 22:54:08 +0200

    implement language in route map list

--- src/bll/map_list.rs
+++ src/bll/map_list.rs
@@ -3,6 +3,6 @@ use tokio_postgres::Error;
 
 use crate::{bo::map::Map, dal::map_list::map_list as dal_map_list};
 
-pub async fn map_list() -> Result<Json<Vec<Map>>, Error> {
-    dal_map_list(String::from("fr-FR")).await.and_then(|res| Ok(Json(res)))
+pub async fn map_list(language : String) -> Result<Json<Vec<Map>>, Error> {
+    dal_map_list(language).await.and_then(|res| Ok(Json(res)))
 }
--- src/routes/map_list.rs
+++ src/routes/map_list.rs
@@ -3,8 +3,10 @@ use rocket::serde::json::Json;
 use crate::bll::map_list::map_list;
 use crate::bo::map::Map;
 
+use super::guards::language_guard::Language;
+
 // @TODO deal with error 500
 #[get("/map/list")]
-pub async fn get_map_list() -> Json<Vec<Map>> {
-    map_list().await.unwrap()
+pub async fn get_map_list(language: Language) -> Json<Vec<Map>> {
+    map_list(String::from(language.0)).await.unwrap()
 }
