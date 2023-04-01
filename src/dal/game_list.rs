40f267063a0fef2f34513958448e40d714379570
ArnoBidet <arno.bidet479@gmail.com>
2023-04-01 14:31:15 +0200

    renommage des view pour quels aient le prefix v_

--- src/dal/game_list.rs
+++ src/dal/game_list.rs
@@ -5,7 +5,7 @@ use crate::bo::game::Game;
 use crate::dal::query::query;
 
 pub async fn game_list() -> Result<Vec<Game>, Error> {
-    let sql_query = "SELECT id, play_count FROM game_list;";
+    let sql_query = "SELECT id, play_count FROM v_game_list;";
     let params  = &[];
     match query(sql_query,params).await {
         Ok(rows)=> Ok(rows_to_game(rows)),
