32e21a26a467c86cbcf731aededf077379170d01
ArnoBidet <arno.bidet479@gmail.com>
2023-04-02 21:33:57 +0200

    added route average awareness

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate rocket;
 use dotenvy::dotenv;
 use rocket::launch;
+use routes::average_awareness::get_average_awareness;
 use routes::game_list::get_game_list;
 use routes::get_map_geo_data::get_map_geo_data;
 use routes::map_list::get_map_list;
@@ -9,23 +10,30 @@ use routes::map_list::get_map_list;
 mod translation_parser;
 
 mod bll {
+    pub mod average_awareness;
     pub mod game_list;
-    pub mod map_list;
     pub mod map_geo_data;
+    pub mod map_list;
 }
 mod bo {
+    pub mod average_awareness;
     pub mod game;
-    pub mod map;
     pub mod geo_data;
+    pub mod map;
 }
 mod dal {
+    pub mod average_awareness;
     pub mod establish_connection;
     pub mod game_list;
-    pub mod map_list;
     pub mod map_geo_data;
+    pub mod map_list;
     pub mod query;
 }
 mod routes {
+    pub mod utils {
+        pub mod lang_utils;
+    }
+    pub mod average_awareness;
     pub mod game_list;
     pub mod get_map_geo_data;
     pub mod guards;
@@ -38,7 +46,10 @@ fn rocket() -> _ {
     dotenv().expect(".env file not found");
     // @TODO extract to another file ?
     // Add support for 404 error
-    rocket::build().mount("/", routes![get_game_list, get_map_list, get_map_geo_data])
+    rocket::build().mount("/", routes![get_game_list,
+    get_map_list,
+    get_map_geo_data,
+    get_average_awareness])
 }
 
 #[cfg(test)]
@@ -104,6 +115,28 @@ mod main_tests {
             response.headers().get_one("content-type").unwrap(),
             "application/json"
         );
+        assert_eq!(
+            response.headers().get_one("Content-Language").unwrap(),
+            "fr-FR"
+        );
+    }
+
+    #[test]
+    fn get_average_awareness() {
+        let client = get_client();
+        let response = client
+            .get(format!(
+                "/average-awareness/{}/{}?id_lang={}",
+                "FRANCE_DEPARTMENTS", "AGAINST_CLOCK", "fr-FR"
+            ))
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        assert_eq!(
+            response.headers().get_one("content-type").unwrap(),
+            "application/json"
+        );
+
         assert_eq!(
             response.headers().get_one("Content-Language").unwrap(),
             "fr-FR"
