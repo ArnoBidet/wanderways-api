8310d274013c0e371cdcf220ae9dfd78fc5805c3
ArnoBidet <arno.bidet479@gmail.com>
2023-04-02 13:59:53 +0200

    optimized function to get tags with language

--- sql/functions/tag_list/up.sql
+++ sql/functions/tag_list/up.sql
@@ -9,21 +9,12 @@ RETURNS TABLE (
 ) 
 language plpgsql
 as $$
-declare 
-    var_r record;
-begin
-	for var_r in(
-            SELECT mtl.id_tag,
-            mtl.label,
-            mtl.id_group,
-            mtl.group_label
-            FROM mv_tag_list mtl
-            WHERE mtl.id_lang = param_id_lang
-        ) loop
-        id_tag := var_r.id_tag;
-        label := var_r.label;
-        id_group := var_r.id_group;
-        group_label := var_r.group_label;
-        return next;
-	end loop;
-end; $$ 
+BEGIN
+	RETURN QUERY
+        SELECT mtl.id_tag,
+        mtl.label,
+        mtl.id_group,
+        mtl.group_label
+        FROM mv_tag_list mtl
+        WHERE mtl.id_lang = param_id_lang;
+END; $$ 
