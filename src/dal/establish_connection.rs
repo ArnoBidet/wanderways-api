0f9c964794ee7a96a536afe7a4b249afc45ec167
ArnoBidet <arno.bidet479@gmail.com>
2023-05-21 22:50:23 +0200

    usage of unix sockets for postgres connection

--- src/dal/establish_connection.rs
+++ src/dal/establish_connection.rs
@@ -5,23 +5,24 @@ use tokio_postgres::{Config, NoTls};
 
 pub async fn establish_connection() -> Result<(Client, JoinHandle<()>), Error> {
     // Connect to the database.
-    let (client, connection) = Config::new()
+    match Config::new()
         .host(&env::var("DB_HOST").unwrap())
         .user(&env::var("DB_USER").unwrap())
-        .port(env::var("DB_PORT").unwrap().parse::<u16>().unwrap())
         .password(env::var("DB_PASS").unwrap())
         .dbname(&env::var("DB_NAME").unwrap())
         .connect(NoTls)
-        .await
-        .unwrap();
+        .await {
+            Ok((client, connection))=>{
+                // The connection object performs the actual communication with the database,
+                // so spawn it off to run on its own.
+                let handle = tokio::spawn(async move {
+                    if let Err(e) = connection.await {
+                        eprintln!("connection error: {}", e);
+                    }
+                });
 
-    // The connection object performs the actual communication with the database,
-    // so spawn it off to run on its own.
-    let handle = tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    Ok((client, handle))
+                Ok((client, handle))
+            },
+            Err(err)=>Err(err)
+         }
 }
